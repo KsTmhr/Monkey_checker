6911965cb8b012055a4a61ccb79019de48111986
KsTmhr <sarutomomo@gmail.com>
2020-11-24 19:48:38 +0900

    MonkeyChecker Ver.0.3 Kishi 画像キャプチャ機能追加(撮影) 検出画面完成？

--- monkey_checker/static/js/webCam.js
+++ monkey_checker/static/js/webCam.js
@@ -2,11 +2,20 @@ const w = document.documentElement.clientWidth;
 const h = document.documentElement.clientHeight;
 
 const resolution = { w: w, h: h };
+
 let video;
+let img;
+let btn_text;
 let media;
+
+let toggleStopButton = 0;
 
 // video要素をつくる
 video = document.getElementById('video');
+img = document.getElementById('img');
+btn_text = document.getElementById('stop_btn').firstChild;
+
+img.style.display = 'none';
 
 // video要素にWebカメラの映像を表示させる
 media = navigator.mediaDevices.getUserMedia({
@@ -17,4 +26,39 @@ media = navigator.mediaDevices.getUserMedia({
   }
 }).then(function(stream) {
   video.srcObject = stream;
-});
+}).then(()=>{ // enable the button
+  const btn = document.getElementById('stop_btn');
+  btn.disabled = false;
+  btn.onclick = e => {
+    if(toggleStopButton == 0){
+      takeASnap()
+      .then(stop);
+      toggleStopButton = 1;
+    }else{
+      start();
+      toggleStopButton = 0;
+    }
+  };
+})
+
+function takeASnap(){
+  const canvas = document.createElement('canvas'); // create a canvas
+  const ctx = canvas.getContext('2d'); // get its context
+  canvas.width = video.videoWidth; // set its size to the one of the video
+  canvas.height = video.videoHeight;
+  ctx.drawImage(video, 0,0); // the video
+  return new Promise((res, rej)=>{
+    canvas.toBlob(res, 'image/jpeg'); // request a Blob from the canvas
+  });
+}
+function stop(blob){
+  video.style.display = 'none';
+  img.src = URL.createObjectURL(blob);
+  img.style.display = 'block'
+  btn_text.data = '撮り直す'
+}
+function start(){
+  video.style.display = 'block';
+  img.style.display = 'none';
+  btn_text.data = 'ストップ'
+}
